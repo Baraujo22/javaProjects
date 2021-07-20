1e5d05cc55bf0dfb8d778f664f356f8c5d31bf70
Brian Araujo <baraujo22@yahoo.com>
2021-07-19 22:07:17 -0400

    lab12a

---
+++ src/LAB12A.java
@@ -0,0 +1,16 @@
+package javaProjects.m1a1.src;
+import java.util.*;
+public class LAB12A {
+    public static void main(String[] args) {
+
+//    For this exercise, create a 1D array full of integers.
+//    Assume the user will input positive integers between 1 and 100.
+//    Once the array has been filled, add the values stored at each index to create a sum.
+
+for(int i = 0; i <= indexBaby; i++){
+        System.out.println("");
+
+}
+
+    }
+}
